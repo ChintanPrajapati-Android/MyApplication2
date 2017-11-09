bb5dc04d8df725e5b89aa77438a26d2babd9f884
Chintan <chinatn.prajapati94@gmail.com>
2017-11-09 23:22:30 +0530

    test

---
+++ app/src/main/java/com/example/chintan/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.chintan.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
